29f4142cdb15a9c4f93e0cab7ac5a73f6829a72a
MaxwellW32 <121204303+MaxwellW32@users.noreply.github.com>
2024-11-30 16:00:25 -0500

    Squaremaxtech 2024-11-30T21:00:19.903Z

---
+++ components/logo/Logo.tsx
@@ -0,0 +1,10 @@
+import { globalFormData } from '@/globalFormData'
+import Image from 'next/image'
+import React from 'react'
+
+export default function Logo() {
+
+    return (
+        <Image alt='logo' src={globalFormData.linkedData.siteInfo.logo} height={140} width={140} style={{ objectFit: "contain" }} />
+    )
+}
