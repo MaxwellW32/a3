1865ae5ed0218a3b4f7e73d960c3a31b8b1ce569
MaxwellW32 <121204303+MaxwellW32@users.noreply.github.com>
2024-11-30 16:00:26 -0500

    Squaremaxtech 2024-11-30T21:00:19.916Z

---
+++ components/contact/ContactUs.tsx
@@ -0,0 +1,51 @@
+import { globalFormData } from '@/globalFormData'
+import Image from "next/image";
+import styles from "./styles.module.css"
+import Socials from "../socials/Socials";
+import { contactComponentType } from "@/types";
+import React from "react"
+
+export default function ContactUs({ contacts }: { contacts: contactComponentType["component"] }) {
+
+    return (
+        <div style={{ display: "flex", flexWrap: "wrap", gap: "vaR(--gapSmall)", justifyContent: "center", alignItems: "center", width: "min(1000px, 100%)", margin: "0 auto" }}>
+
+            {globalFormData.specificData.pages.home.section1.fieldType === "section" && globalFormData.specificData.pages.home.section1.inputs.image2.fieldType === "image" && (
+                <div style={{ flex: "0 0 auto", position: "relative", width: "min(250px, 80vw)", aspectRatio: "1/1" }}>
+                    <Image alt={globalFormData.specificData.pages.home.section1.inputs.image2.alt} src={globalFormData.specificData.pages.home.section1.inputs.image2.value} fill={true} sizes="(max-width: 200px) 100vw, (max-width: 600px) 50vw, 33vw" style={{ objectFit: "cover", borderRadius: "var(--radiusAmountLarge)", }} />
+                </div>
+            )}
+
+
+            <div style={{ flex: "1 1 300px", backgroundColor: "var(--bg1)", display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(min(300px, 100%), 1fr))" }}>
+                {contacts.map((eachContactInfo, eachContactInfoIndex) => {
+                    return (
+                        <div key={eachContactInfoIndex} className={styles.contact}>
+                            <div className={styles.iconCont}>
+                                <svg
+                                    dangerouslySetInnerHTML={{ __html: eachContactInfo.svg.value }}
+                                />
+                            </div>
+
+                            <div className={styles.infoCont}>
+                                <h3>{eachContactInfo.title.value}</h3>
+
+                                {eachContactInfo.texts.length > 0 && (
+                                    <ul className={styles.textCont}>
+                                        {eachContactInfo.texts.map((eachText, eachTextIndex) => {
+                                            return (
+                                                <li key={eachTextIndex}>{eachText.value}</li>
+                                            )
+                                        })}
+                                    </ul>
+                                )}
+                            </div>
+                        </div>
+                    )
+                })}
+
+                <Socials />
+            </div>
+        </div>
+    )
+}
